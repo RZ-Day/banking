c6983b0bedfec118885a37939832c6e82d19fa98
Ryan Day <ryan0699@gmail.com>
2024-08-21 22:02:49 -0400

    implement navigation bars for desktop and mobile

---
+++ components/MobileNav.tsx
@@ -0,0 +1,74 @@
+'use client'
+
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { usePathname } from 'next/navigation';
+import { sidebarLinks } from '@/constants';
+import {
+Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet";
+
+const MobileNav = ({ user }: MobileNavProps) => {
+    const pathname = usePathname();
+
+    return (
+    <section className="w-full max-w-[264px]">
+        <Sheet>
+        <SheetTrigger>
+            <Image
+                src="/icons/hamburger.svg"
+                width={30}
+                height={30}
+                alt="menu"
+                className="cursor-pointer"
+            />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-white">
+            <Link href="/" className="flex cursor-pointer items-center gap-1 px-4">
+                <Image
+                    src="/icons/logo.svg"
+                    width={34}
+                    height={34}
+                    alt="Horizon Logo"
+                    />
+                <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Horizon</h1>
+            </Link>
+
+            {
+                sidebarLinks.map((item) => {
+                    const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
+
+                    return (
+                        <Link href={item.route} className={cn('sidebar-link', {'bg-bank-gradient': isActive})} key={item.label}>
+                            <div className="relative size-6">
+                                <Image
+                                    src={item.imgURL}
+                                    alt={item.label}
+                                    fill
+                                    className={cn({'brightness-[3] invert-0': isActive})}
+                                />
+                            </div>
+                            <p className={cn('sidebar-label', {'!text-white': isActive})}>
+                                    {item.label}
+                            </p>
+                        </Link>
+                    );
+                })
+            }
+            
+            USER
+        </SheetContent>
+        </Sheet>
+
+    </section>
+  )
+}
+
+export default MobileNav
